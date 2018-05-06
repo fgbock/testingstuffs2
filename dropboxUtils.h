368b387c8a3964029c66cf46b66f2b95b2596169
lowellausen <lwellausen@gmail.com>
2018-05-06 00:01:14 -0300

    nada

--- dropboxUtils.h
+++ dropboxUtils.h
@@ -4,19 +4,5 @@
 #define MAXNAME 20
 #define MAXFILES 10
 
-struct file_info{
-	char name[MAXNAME];
-	char extension[MAXNAME];
-	char last_modified[MAXNAME];
-	int size;
-};
-
-struct client{
-	int devices[2];
-	char userid[MAXNAME];
-	struct file_info fi[MAXFILES];
-	int logged_in;
-};
-
 
 #endif
