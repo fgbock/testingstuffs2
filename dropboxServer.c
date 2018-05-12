b9c4fef496e521ad5d211a9ab7f5a8b8fd3196b7
Fenris <fernandobock1996@gmail.com>
2018-05-12 20:55:22 -0300

    meme

--- dropboxServer.c
+++ dropboxServer.c
@@ -22,7 +22,7 @@ struct file_info {
 struct client {
   int devices[2];
   struct sockaddr sender;
-  int session_socket;
+  char client_buffer[1250];
   char userid [MAXNAME];
   struct file_info info [MAXFILES];
   int logged_in;
@@ -42,7 +42,6 @@ void *session_manager(void *args){
 	session_client = args;
 	printf("\n\nNew session created...\n");
 	printf("User ID is: %s\n", session_client->userid);
-	printf("Session socket is: %d\n\n", session_client->session_socket);
 	return 0;
 }
 
@@ -50,8 +49,10 @@ int main(int argc,char *argv[]){
 	struct sockaddr_in server;
 	struct sockaddr client;
 	SOCKET s_socket;
-	int server_len, received, i, online = 1, client_len = sizeof(struct sockaddr_in), client_counter = 0;
+	int server_len, received, i, online = 1, client_len = sizeof(struct sockaddr_in), session_counter = 0;
 	char packet_buffer[1250];
+	char op_code[6];
+	char aux_username[20];
 
 	if ((s_socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		printf("ERROR: Socket creation failure.\n");
@@ -82,30 +83,24 @@ int main(int argc,char *argv[]){
 		received = recvfrom(s_socket,packet_buffer,sizeof(packet_buffer),0,(struct sockaddr *) &client,(socklen_t *)&client_len);
 		if (!received){
 			printf("ERROR: Package reception error.\n\n");
-			exit(2);
 		}
-		else if (received){//switch to check if op is login
-			client_counter++;
+		for (i = 0; i < 4; i++){
+			op_code[i] = packet_buffer[i];
+		}
+		if (strcmp(op_code,"logins")){
+			for (i = 0; i < 20; i++){
+				aux_username[i] = packet_buffer[10+i];
+			}
+			session_counter++;
 			sendto(s_socket,"ACK",sizeof("ACK"),0,(struct sockaddr *)&client, client_len);
-			// ^ reply with session_manager's port instead ^
 			pthread_t tid;
 			struct client new_client;
-			strcpy(new_client.userid,"teste");// switch to actually getting the user from the packet
+			strcpy(new_client.userid, aux_username);
 			new_client.sender = client;
-			// Let's bind a new socket! yay!
-			struct sockaddr_in session;
-			SOCKET session_socket;
-			int new_port = client_counter + MAIN_PORT;
-			memset((void *) &session,0,sizeof(struct sockaddr_in));
-			session.sin_family = AF_INET;
-			session.sin_addr.s_addr = htonl(INADDR_ANY);
-			session.sin_port = htons(new_port); 
-			int session_len = sizeof(session);
-			if(bind(session_socket,(struct sockaddr *) &session, session_len)) {
-				printf("Binding error\n");
-				exit(1);
-			}
 			pthread_create(&tid, NULL, session_manager, &new_client);
+		}
+		else{
+			printf("Redirecting packet to session manager...");
 		}
 	}
 	return 0;
