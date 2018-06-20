a360329d36f413c251a9ca12c1cc7c4fec4d2287
fgbock <fernandobock1996@gmail.com>
2018-06-20 17:43:11 -0300

    lamad

--- frontEnd.c
+++ frontEnd.c
@@ -1,3 +1,6 @@
 int main(){
+  // Start thread to ping primary and if tout elect new primary
+  // Start thread to receive from client and send to server
+  // Start thread to receive from server and send to client
   return 0;
 }
